a5119c300627cfb7939a248f79556509fb951c7c
Brian-Lee <BrianTrewLee@gmail.com>
2019-09-18 11:30:57 -0700

    added a func to brians_test_file

--- brians_test_file.py
+++ brians_test_file.py
@@ -10,3 +10,8 @@ def test_func2():
 def test_func3():
     ''' this function was already documented'''
     this_function_doesnt_really_do_much = True
+
+def test_func4(input):
+	#this is test_func4
+	print(input)
+	retun True
