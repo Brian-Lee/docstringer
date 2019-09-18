8a9081b564da93e7035389e8872457ed1bc41626
Brian-Lee <BrianTrewLee@gmail.com>
2019-09-18 12:10:43 -0700

    detecting some needed docstrings and putting them in

--- docstringer.py
+++ docstringer.py
@@ -2,27 +2,32 @@
 # Embarass yourself into adding real docstrings
 
 filename = 'brians_test_file.py'
-docstring = "'''BrianIsAnidiot'''"
+docstring = "'''BrianIsAnidiot'''\n"
 new_lines = []
 
 def is_function_def(line):
-    return True
+    if(('def ' in line) and (':' in line)):
+        return True
 
 
 def is_not_docstring(line):
-    return True
-
+    if ("'''" not in line):
+        return True
 
 with(open(filename,'r')) as f:
     lines = f.readlines()
 
     line_above = '' #keep track of the previous line - initialize empty
     for line in lines:
-        new_lines.append(line)
+
         if(is_function_def(line_above) and is_not_docstring(line)):
             new_lines.append(docstring)
+        new_lines.append(line)
+        line_above = line
 f.close()
 
-print(new_lines)
-# eventually we need to write new_lines back to filename
-#with(open(filename,'w')) as f:
+#out_filename = 'outfile.py'
+out_filename = filename
+out_file = open(out_filename, "w")
+out_file.writelines(new_lines)
+out_file.close()
